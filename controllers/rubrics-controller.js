0d28077aa2369d964b30ea27eaac8501bdd3fc6d
Ansel Bridgewater <iamansel@gmail.com>
2019-03-26 16:35:40 -0700

    Added full rubric route

--- controllers/rubrics-controller.js
+++ controllers/rubrics-controller.js
@@ -24,6 +24,101 @@ module.exports = (app) => {
     })
   });
 
+  // Create a complete, nested rubric
+  app.post('/users/:userId/completeRubrics/create', async function(req, res) {
+    const newRubric = {...req.body.rubric, userId: req.params.userId}
+    const rubric = await db.Rubric.create(newRubric)
+    .then((rubric) => {
+      return rubric
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400);
+      res.json({
+        message: "Error!",
+        error: err
+      })
+    })
+
+    const competencies = await Promise.all(
+      req.body.competencies[0].map(async (competency, index) => {
+
+        const newCompetency = {name: competency, rubricId: rubric.id}
+        return db.Competency.create(newCompetency)
+        .then((competency) => {
+          return competency.dataValues
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400);
+          res.json({
+            message: "Error!",
+            error: err
+          })
+        })
+
+      })
+    )
+
+    const scales = await Promise.all(
+      req.body.skills.map(async (skillsGroup, compIndex) => {
+        return await Promise.all(
+          skillsGroup.map(async (skill, index) => {
+
+            const newScale = {name: skill, competencyId: competencies[compIndex].id}
+            return db.Scale.create(newScale)
+            .then((scale) => {
+              console.log("data123", scale.dataValues)
+              return scale.dataValues
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(400);
+              res.json({
+                message: "Error!",
+                error: err
+              })
+            })
+
+          })
+        )
+      })
+    )
+
+    const scalesFlat = scales.reduce((acc, scaleGroup) => [...acc, ...scaleGroup])
+    console.log(scalesFlat)
+
+    const criterion = await Promise.all(
+      req.body.evaluations.map(async (evaluationsGroup, skillIndex) => {
+        return await Promise.all(
+          evaluationsGroup.map(async (evaluation, level) => {
+
+            const newCriterion = {text: evaluation, level: level + 1, scaleId: scalesFlat[skillIndex].id}
+            console.log(newCriterion)
+            return db.Criterion.create(newCriterion)
+            .then((criterion) => {
+              return criterion.dataValues
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(400);
+              res.json({
+                message: "Error!",
+                error: err
+              })
+            })
+
+          })
+        )
+      })
+    )
+
+    res.status(200)
+    res.json({
+      message: 'Completed rubric added successfully!'
+    })
+  });
+
   // Index of all Rubrics
   app.get('/rubrics', (req, res) => {
     db.Rubric.findAll()
